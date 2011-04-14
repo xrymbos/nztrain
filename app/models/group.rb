7110be831522926ea2d7ffe0197415214d2444c0
Kitten <kitten.tofu@gmail.com>
2011-04-15 01:26:07 +1000

    fixed group setting via white separated email list

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,11 +2,10 @@ class Group < ActiveRecord::Base
   has_and_belongs_to_many :users
 
   def emails_as_text=(emails)
-    users = User.where(:email => emails.split)
-    print "got users = " + (users.to_s) + "\n"
+    self.users = User.where(:email => emails.split)
   end
 
   def emails_as_text
-    users.to_s
+    users.map{ |u| u.email }.join(" ")
   end
 end
